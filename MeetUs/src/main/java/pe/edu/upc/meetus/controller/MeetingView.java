4a8757ac7d137c7c34f6ee0d7b7e0c90298ea4a3
Erick Hennings Montalvo <43503341+erick2904@users.noreply.github.com>
2022-05-05 19:00:47 -0500

    La última versión pero por si no abre el zip
    tp

--- MeetUs/src/main/java/pe/edu/upc/meetus/controller/MeetingView.java
+++ MeetUs/src/main/java/pe/edu/upc/meetus/controller/MeetingView.java
@@ -2,26 +2,100 @@ package pe.edu.upc.meetus.controller;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import pe.edu.upc.meetus.business.crud.CrudService;
+import pe.edu.upc.meetus.business.crud.MeetingService;
+import pe.edu.upc.meetus.model.entity.Meeting;
+import pe.edu.upc.meetus.model.entity.Organizer;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named("meetingView")
 @ViewScoped
-public class MeetingView implements Serializable {
+public class MeetingView implements Serializable, EntityView<Meeting, Integer> {
 	private static final long serialVersionUID = 1L;
-	private String text;
 
-	@PostConstruct // Primer método que carga al cargar la página
+	private List<Meeting> entities;
+	private Meeting entitySelected;
+	private List<Meeting> entitiesSelected;
+	private Meeting entitySearch;
+
+	@Inject
+	private MeetingService entityService;
+
+	@PostConstruct
 	public void init() {
-		this.text = "Web Programming";
+		this.entitiesSelected = new ArrayList<>();
+		this.entitySearch = new Meeting();
+		getAllEntities();
 	}
 
-	public String getText() {
-		return text;
+	@Override
+	public CrudService<Meeting, Integer> getCrudService() {
+		return this.entityService;
 	}
 
-	public void setText(String text) {
-		this.text = text;
+	@Override
+	public void createNew() {
+		this.entitySelected = new Meeting();
+		this.entitySelected.setOrganizer(new Organizer());
 	}
 
+	@Override
+	public Integer getIdFromEntitySelected() {
+		return this.entitySelected.getId();
+	}
+
+	@Override
+	public void searchEntity() {
+		try {
+			this.entities = this.entityService.search(this.entitySearch.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Meeting> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(List<Meeting> entities) {
+		this.entities = entities;
+	}
+
+	public Meeting getEntitySelected() {
+		return entitySelected;
+	}
+
+	public void setEntitySelected(Meeting entitySelected) {
+		this.entitySelected = entitySelected;
+	}
+
+	public List<Meeting> getEntitiesSelected() {
+		return entitiesSelected;
+	}
+
+	public void setEntitiesSelected(List<Meeting> entitiesSelected) {
+		this.entitiesSelected = entitiesSelected;
+	}
+
+	public Meeting getEntitySearch() {
+		return entitySearch;
+	}
+
+	public void setEntitySearch(Meeting entitySearch) {
+		this.entitySearch = entitySearch;
+	}
+
+	public MeetingService getEntityService() {
+		return entityService;
+	}
+
+	public void setEntityService(MeetingService entityService) {
+		this.entityService = entityService;
+	}
 }
--- MeetUs/src/main/java/pe/edu/upc/meetus/controller/OrganizerView.java
+++ MeetUs/src/main/java/pe/edu/upc/meetus/controller/OrganizerView.java
@@ -1,29 +1,103 @@
 package pe.edu.upc.meetus.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import pe.edu.upc.meetus.business.crud.CrudService;
+import pe.edu.upc.meetus.business.crud.OrganizerService;
+import pe.edu.upc.meetus.model.entity.Organizer;
 
 import javax.faces.view.ViewScoped;
 
 @Named("organizerView")
 @ViewScoped
-public class OrganizerView implements Serializable{
+public class OrganizerView implements Serializable, EntityView<Organizer, Integer> {
 	private static final long serialVersionUID = 1L;
-	private String text;
+
+	private List<Organizer> entities;
+	private Organizer entitySelected;
+	private List<Organizer> entitiesSelected;
+	private Organizer entitySearch;
+
+	@Inject
+	private OrganizerService entityService;
 
 	@PostConstruct // Primer método que carga al cargar la página
 	public void init() {
-		this.text = "Web Programming";
+		this.entitiesSelected = new ArrayList<>();
+		this.entitySearch = new Organizer();
+		getAllEntities();
+
 	}
 
-	public String getText() {
-		return text;
+	@Override
+	public CrudService<Organizer, Integer> getCrudService() {
+		return this.entityService;
 	}
 
-	public void setText(String text) {
-		this.text = text;
+	@Override
+	public void createNew() {
+		this.entitySelected = new Organizer();
+
+	}
+
+	@Override
+	public Integer getIdFromEntitySelected() {
+		return this.entitySelected.getId();
+	}
+
+	@Override
+	public void searchEntity() {
+		try {
+			this.entities = this.entityService.search(this.entitySearch.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Organizer> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(List<Organizer> entities) {
+		this.entities = entities;
+	}
+
+	public Organizer getEntitySelected() {
+		return entitySelected;
+	}
+
+	public void setEntitySelected(Organizer entitySelected) {
+		this.entitySelected = entitySelected;
+	}
+
+	public List<Organizer> getEntitiesSelected() {
+		return entitiesSelected;
+	}
+
+	public void setEntitiesSelected(List<Organizer> entitiesSelected) {
+		this.entitiesSelected = entitiesSelected;
+	}
+
+	public Organizer getEntitySearch() {
+		return entitySearch;
+	}
+
+	public void setEntitySearch(Organizer entitySearch) {
+		this.entitySearch = entitySearch;
+	}
+
+	public OrganizerService getEntityService() {
+		return entityService;
+	}
+
+	public void setEntityService(OrganizerService entityService) {
+		this.entityService = entityService;
 	}
 
 }
